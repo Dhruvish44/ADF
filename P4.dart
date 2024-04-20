a5f86315be92b52f91a86e3f32bfcebbd3170f19
Dhruvish44 <154946541+Dhruvish44@users.noreply.github.com>
2024-04-20 19:28:52 +0530

    Update P4.dart

--- P4.dart
+++ P4.dart
@@ -1,1 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('3x3 Layout Example'),
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    _buildItem('Row 1, Column 1'),
+                    _buildItem('Row 1, Column 2'),
+                    _buildItem('Row 1, Column 3'),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    _buildItem('Row 2, Column 1'),
+                    _buildItem('Row 2, Column 2'),
+                    _buildItem('Row 2, Column 3'),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    _buildItem('Row 3, Column 1'),
+                    _buildItem('Row 3, Column 2'),
+                    _buildItem('Row 3, Column 3'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItem(String text) {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      color: Colors.blue,
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
 
