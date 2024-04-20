9c298d3497b8f8742e898702a13dd1c6e8e8f8c0
Dhruvish44 <154946541+Dhruvish44@users.noreply.github.com>
2024-04-20 19:31:50 +0530

    Create P7.dart

---
+++ P7.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Navigation Example',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/second': (context) => SecondScreen(),
+        '/third': (context) => ThirdScreen(),
+      },
+      // Define names for routes
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (context) => HomeScreen(), settings: settings);
+          case '/second':
+            return MaterialPageRoute(builder: (context) => SecondScreen(), settings: settings);
+          case '/third':
+            return MaterialPageRoute(builder: (context) => ThirdScreen(), settings: settings);
+          default:
+            return null;
+        }
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to the second screen
+            Navigator.pushNamed(context, '/second');
+          },
+          child: Text('Go to Second Screen'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the third screen
+                Navigator.pushNamed(context, '/third');
+              },
+              child: Text('Go to Third Screen')
+
+            ),
+
+            SizedBox(height: 20),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Third Screen'),
+      ),
+      body: Center(
+        child: Text('Dhruvish'),
+      ),
+    );
+  }
+}
