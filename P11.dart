ea12b6a7bbe44d1f7502017ad57c0131388d7b39
Dhruvish44 <154946541+Dhruvish44@users.noreply.github.com>
2024-04-20 19:42:10 +0530

    Update P11.dart

--- P11.dart
+++ P11.dart
@@ -1,125 +1,135 @@
 import 'package:flutter/material.dart';
-
+import 'package:resetapi/data_screen.dart';
 void main() {
-  runApp(const MyApp());
+runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+debugShowCheckedModeBanner: false,
+title: 'Flutter REST API Demo',
+theme: ThemeData(
+primarySwatch: Colors.blue,
+),
+home: DataScreen(),
+);
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+api_service.dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+class Post {
+final int userId;
+final int id;
+final String title;
+final String body;
+Post({
+required this.userId,
+required this.id,
+required this.title,
+required this.body,
+});
+factory Post.fromJson(Map<String, dynamic> json) {
+return Post(
+userId: json['userId'],
+id: json['id'],
+title: json['title'],
+body: json['body'],
+);
 }
+FACULTY OF ENGINEERING AND TECHNOLOGY
+Department of Computer Engineering
+App Development Using Flutter (01CE0610)
+92210103061 6TC3-C
+}
+class ApiService {
+static const String baseUrl = 'https://jsonplaceholder.typicode.com/todos/1';
+static Future<List<Post>> fetchPosts() async {
+final response = await http.get(Uri.parse('$baseUrl/posts'));
+if (response.statusCode == 200) {
+List<dynamic> jsonResponse = json.decode(response.body);
+return jsonResponse.map((post) => Post.fromJson(post)).toList();
+} else {
+throw Exception('Failed to load posts');
+}
+}
+}
+data_screen.dart
+import 'package:flutter/material.dart';
+import 'package:resetapi/api_service.dart';
+class DataScreen extends StatefulWidget {
+@override
+_DataScreenState createState() => _DataScreenState();
+}
+class _DataScreenState extends State<DataScreen> {
+late Future<List<Post>> posts;
+@override
+void initState() {
+super.initState();
+posts = ApiService.fetchPosts();
+}
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: Text('Posts'),
+),
+body: Center(
+child: FutureBuilder<List<Post>>(
+future: posts,
+builder: (context, snapshot) {
+if (snapshot.hasData) {
+return ListView.builder(
+itemCount: snapshot.data!.length,
+itemBuilder: (context, index) {
+return Card(
+FACULTY OF ENGINEERING AND TECHNOLOGY
+Department of Computer Engineering
+App Development Using Flutter (01CE0610)
+92210103061 6TC3-C
+elevation: 3,
+margin: EdgeInsets.all(10),
+child: Padding(
+padding: EdgeInsets.all(10),
+child: Column(
+crossAxisAlignment: CrossAxisAlignment.start,
+children: [
+Text(
+'Post ${index + 1}:', // Add label here
+style: TextStyle(
+fontWeight: FontWeight.bold,
+fontSize: 16,
+),
+),
+SizedBox(height: 5),
+Text(
+snapshot.data![index].title,
+style: TextStyle(
+fontWeight: FontWeight.bold,
+fontSize: 18,
+),
+),
+SizedBox(height: 5),
+Text(snapshot.data![index].body),
+],
+),
+),
+);
+},
+);
+} else if (snapshot.hasError) { return
+Text("${snapshot.error}");
+}
+// By default, show a loading spinner.
+return CircularProgressIndicator();
+},
+),
+),
+);
+}
+}
+dev_dependencies:
+flutter_test:
+sdk: flutter http:
+^0.13.
